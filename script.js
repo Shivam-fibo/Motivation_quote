0b194c42fa7a7e9ed4bf5fbef32b95fab0ef5fd5
Shivam Kumar Gupta <115231608+Shivam-fibo@users.noreply.github.com>
2023-06-11 20:54:24 +0530

    Simple website which generate motivation quote

---
+++ script.js
@@ -0,0 +1,134 @@
+const data = [
+    
+        
+           { quote: 'Success is not final, failure is not fatal: It is the courage to continue that counts.'},
+           { quote: 'The only limit to our realization of tomorrow will be our doubts of today.'},
+           { quote: "Don't watch the clock; do what it does. Keep going."},
+           { quote: 'Believe you can and you\'re halfway there.'},
+           { quote: 'The harder you work for something, the greater you\'ll feel when you achieve it.'},
+           { quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart."},
+           { quote: 'Success is not in what you have, but who you are.'},
+           { quote: 'The only way to do great work is to love what you do.'},
+           { quote: 'The future belongs to those who believe in the beauty of their dreams.'},
+           { quote: 'Success usually comes to those who are too busy to be looking for it.'},
+           { quote: 'Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.'},
+           { quote: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.'},
+           { quote: "Your time is limited, don't waste it living someone else's life."},
+           { quote: 'Don\'t be afraid to give up the good to go for the great.'},
+           { quote: 'The only person you are destined to become is the person you decide to be.'},
+           { quote: 'Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.'},
+           { quote: 'The best way to predict the future is to create it.'},
+           { quote: 'The secret of getting ahead is getting started.'},
+           { quote: 'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.'},
+           { quote: 'The road to success and the road to failure are almost exactly the same.'},
+           { quote: 'The only limit to our realization of tomorrow will be our doubts of today.'},
+           { quote: 'The mind is everything. What you think you become.'},
+           { quote: 'The only thing standing between you and your goal is the story you keep telling yourself as to why you can\'t achieve it.'},
+           { quote: 'Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got this.'},
+           { quote: 'Success is not the absence of failure; it\'s the persistence through failure.'},
+           { quote: 'The only way to do great work is to love what you do.'},
+           { quote: 'The past cannot be changed. The future is yet in your power.'},
+           { quote: 'The best revenge is massive success.'},
+           { quote: 'If you want to achieve greatness stop asking for permission.'},
+           { quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart."},
+           { quote: 'The only place where success comes before work is in the dictionary.'},
+           { quote: 'The biggest risk is not taking any risk. In a world that is changing quickly, the only strategy that is guaranteed to fail is not taking risks.'},
+           { quote: 'If you really want to do something, you\'ll find a way. If you don\'t, you\'ll find an excuse.'},
+           { quote: 'The only limit to our realization of tomorrow will be our doubts of today.'},
+           { quote: "You can't use up creativity. The more you use, the more you have."},
+           { quote: 'I find that the harder I work, the more luck I seem to have.'},
+           { quote: 'The only thing standing between you and your goal is the story you keep telling yourself as to why you can\'t achieve it.'},
+           { quote: 'The successful warrior is the average man, with laser-like focus.'},
+           { quote: "Don't let yesterday take up too much of today."},
+           { quote: 'The man who has confidence in himself gains the confidence of others.'},
+           { quote: 'The future belongs to those who believe in the beauty of their dreams.'},
+           { quote: 'Success usually comes to those who are too busy to be looking for it.'},
+           { quote: 'The best way to predict the future is to create it.'},
+           { quote: "Don't be afraid to give up the good to go for the great."},
+           { quote: 'The only person you are destined to become is the person you decide to be.'},
+           { quote: 'The harder you work for something, the greater you\'ll feel when you achieve it.'},
+           { quote: 'Believe you can and you\'re halfway there.'},
+           { quote: 'The successful warrior is the average man, with laser-like focus.'},
+           { quote: "Your time is limited, don't waste it living someone else's life."},
+           { quote: 'Success is not in what you have, but who you are.'},
+           { quote: 'Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.'},
+           { quote: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.'},
+           { quote: 'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.'},
+           { quote: 'The only limit to our realization of tomorrow will be our doubts of today.'},
+           { quote: 'The mind is everything. What you think you become.'},
+           { quote: 'The only thing standing between you and your goal is the story you keep telling yourself as to why you can\'t achieve it.'},
+           { quote: 'Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got this.'},
+           { quote: 'Success is not the absence of failure; it\'s the persistence through failure.'},
+           { quote: 'The only way to do great work is to love what you do.'},
+           { quote: 'The past cannot be changed. The future is yet in your power.'},
+           { quote: 'The best revenge is massive success.'},
+           { quote: 'If you want to achieve greatness stop asking for permission.'},
+           { quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart."},
+           { quote: 'The only place where success comes before work is in the dictionary.'},
+           { quote: 'The biggest risk is not taking any risk. In a world that is changing quickly, the only strategy that is guaranteed to fail is not taking risks.'},
+           { quote: 'If you really want to do something, you\'ll find a way. If you don\'t, you\'ll find an excuse.'},
+           { quote: 'The only limit to our realization of tomorrow will be our doubts of today.'},
+           { quote: "You can't use up creativity. The more you use, the more you have."},
+           { quote: 'I find that the harder I work, the more luck I seem to have.'},
+           { quote: 'The only thing standing between you and your goal is the story you keep telling yourself as to why you can\'t achieve it.'},
+           { quote: 'The successful warrior is the average man, with laser-like focus.'},
+           { quote: "Don't let yesterday take up too much of today."},
+           { quote: 'The man who has confidence in himself gains the confidence of others.'},
+           { quote: 'The future belongs to those who believe in the beauty of their dreams.'},
+           { quote: 'Success usually comes to those who are too busy to be looking for it.'},
+           { quote: 'The best way to predict the future is to create it.'},
+           { quote: "Don't be afraid to give up the good to go for the great."},
+           { quote: 'The only person you are destined to become is the person you decide to be.'},
+           { quote: 'The harder you work for something, the greater you\'ll feel when you achieve it.'},
+           { quote: 'Believe you can and you\'re halfway there.'},
+           { quote: 'The successful warrior is the average man, with laser-like focus.'},
+           { quote: "Your time is limited, don't waste it living someone else's life."},
+           { quote: 'Success is not in what you have, but who you are.'},
+           { quote: 'Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.'},
+           { quote: 'Challenges are what make life interesting and overcoming them is what makes life meaningful.'},
+           { quote: 'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.'},
+           { quote: 'The only limit to our realization of tomorrow will be our doubts of today.'},
+           { quote: 'The mind is everything. What you think you become.'},
+           { quote: 'The only thing standing between you and your goal is the story you keep telling yourself as to why you can\'t achieve it.'},
+           { quote: 'Believe in yourself, take on your challenges, dig deep within yourself to conquer fears. Never let anyone bring you down. You got this.'},
+           { quote: 'Success is not the absence of failure; it\'s the persistence through failure.'},
+           { quote: 'The only way to do great work is to love what you do.'},
+           { quote: 'The past cannot be changed. The future is yet in your power.'},
+           { quote: 'The best revenge is massive success.'},
+           { quote: 'If you want to achieve greatness stop asking for permission.'},
+           { quote: "Don't be pushed around by the fears in your mind. Be led by the dreams in your heart."},
+           { quote: 'The only place where success comes before work is in the dictionary.'},
+           { quote: 'The biggest risk is not taking any risk. In a world that is changing quickly, the only strategy that is guaranteed to fail is not taking risks.'},
+           { quote: 'If you really want to do something, you\'ll find a way. If you don\'t, you\'ll find an excuse.'},
+           { quote: 'The only limit to our realization of tomorrow will be our doubts of today.'}
+          
+          
+    
+]
+function iterate(quotes){
+    let nextIndex = 0;
+    return {
+        next: function(){
+            return nextIndex<quotes.length?
+            {  value : quotes[nextIndex++], done : false}:
+            {done: true}
+        }
+    }
+}
+const motivation = iterate(data);
+
+nextData();
+
+let btn = document.getElementById('btn')
+btn.addEventListener('click', nextData);
+
+function nextData(){
+    const currentData = motivation.next().value;
+    let quoteContainer = document.getElementById('quoteContainer');
+    if(currentData!=undefined){
+        quoteContainer.innerHTML=`${currentData.quote}`;
+    }
+    else{
+        alert('End of quote');
+    }
+}
